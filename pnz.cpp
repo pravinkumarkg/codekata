507cbca3d5947d3fc0a416f8ea123f985f73923c
pravinkumarkg <35860045+pravinkumarkg@users.noreply.github.com>
2019-07-07 18:28:03 +0530

    Update pnz.cpp

--- pnz.cpp
+++ pnz.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+	int num;
+	cin>>num;
+	if(num==0)
+	cout<<"zero";
+	else if(num<0)
+	cout<<"negative";
+	else
+	cout<<"postive";
+	return 0;
+}
