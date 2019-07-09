0fa19d064ba8632bb935fa8e40f489290f4922dd
pravinkumarkg <35860045+pravinkumarkg@users.noreply.github.com>
2019-07-09 15:41:35 +0530

    Create primeseries.cpp

---
+++ primeseries.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int m,n,i,j,temp,flag;
+   cin>>m>>n;
+   if(m>n)
+   {
+   	temp=m;
+   	m=n;
+   	n=temp;
+   }
+   for(i=m+1;i<n;i++)
+   {
+   	flag=0;
+   	for(j=2;j<=i/2;j++)
+   	{
+   		if(i%j==0)
+   		{
+   			flag=1;
+   			break;
+   		}
+   	}
+   	if(flag==0)
+   	{
+   		cout<<i<<'\t';
+   		
+   	}
+   }
+    return 0;
+}
